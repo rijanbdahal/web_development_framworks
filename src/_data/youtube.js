ebbc930454fbc8ed0a3750d8c3f73a489af5e4b2
rijan <rijan.dahal@dcmail.ca>
2024-12-11 21:51:39 -0500

    FInal Commit 1.2

--- src/_data/youtube.js
+++ src/_data/youtube.js
@@ -1,11 +1,11 @@
 //I am using Axios to make HTTP requests
-
+require('dotenv').config();
 // Importing the axios library for making HTTP requests
 const axios = require('axios');
 console.log("API Req");
 module.exports = async function() {
   try {
-    const response = await axios.get('https://www.googleapis.com/youtube/v3/search?key=AIzaSyCdJTdKTQVmsjuz1mWCHcYlqXqQZHCcmIE&channelId=UC3wNzfbOH9NrX55uYmFSCTA&part=snippet&type=video&order=date&maxResults=5');  
+    const response = await axios.get(`${process.env.GOOGLE_API}`);
     console.log("API Req");
     return response.data;
   } catch (error) {
